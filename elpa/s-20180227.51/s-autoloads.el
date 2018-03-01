1eb1d2f06e06586dce826aaade07d1c0b13d087a
Rich Guilmain <guilmain@isi.edu>
2018-03-01 11:31:22 -0500

    Upgrade s package.
    This may or may not resolve an issue I previously had with
    exec-path-from-shell not running properly.

--- elpa/s-20180227.51/s-autoloads.el
+++ elpa/s-20180227.51/s-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("s.el") (23171 7483 284568 842000))
+;;;### (autoloads nil nil ("s.el") (23192 10787 0 0))
 
 ;;;***
 
